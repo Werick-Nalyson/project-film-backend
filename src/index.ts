fde1ef0633040dcca0b913af75e428fc6f8953fd
Werick Nalyson <wericknalyson@gmail.com>
2022-11-26 00:13:03 -0300

    add documentation

--- src/index.ts
+++ src/index.ts
@@ -4,16 +4,21 @@ import mongoose from 'mongoose';
 import { AppErrorMiddleware } from './shared/http/middlewares/AppError';
 import { routes } from './shared/http/routes';
 import cors from 'cors';
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from './swagger.json';
 
 mongoose.connect('mongodb://localhost:27018')
   .then(() => {
     const app = express();
+
+    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
     app.use(cors());
 
     app.use(express.json());
     app.use(routes);
     app.use(AppErrorMiddleware);
+
 
     const port = 3001;
     app.listen(3001, () => {
